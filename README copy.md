581789de0d3d8b4250e3c63a87d8f923e0ca9547
Dylan Grootenboer <dylancgrooten@gmail.com>
2025-04-30 09:46:40 -0400

    Initial push

---
+++ README copy.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Stupid-Names/pen/azzVQXv](https://codepen.io/Stupid-Names/pen/azzVQXv).
+
---
+++ dist/script.js
@@ -0,0 +1,29 @@
+var element = document.getElementById('the-box')
+var x = 0; var y = 0
+
+interact(element)
+  .draggable({
+    modifiers: [
+      interact.modifiers.snap({
+        targets: [
+          interact.snappers.grid({ x: 30, y: 30 })
+        ],
+        range: Infinity,
+        relativePoints: [ { x: 0, y: 0 } ]
+      }),
+    ],
+    // inertia: true
+  })
+  .on('dragmove', function (event) {
+    x += event.dx
+    y += event.dy
+
+    event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  })
+
+rotationAngle = 0
+interact('.box-class')
+  .on('click', function (event) {
+  rotationAngle = (rotationAngle + 20) % 360;
+    event.currentTarget.style.transform = `rotate(${rotationAngle}deg)`; 
+  });
