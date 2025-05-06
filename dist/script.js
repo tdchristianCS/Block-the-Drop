f5ebb3b32783ab1bcef1811c093511a907432581
Kyle Flowers <kyle.w.flowers@gmail.com>
2025-05-06 14:06:28 -0400

    add boxes

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
-var box = document.getElementById('the-box')
+
+// Box Can Move (via Drag) and Rotate (via Right Click)
+var box = document.getElementById('the-brick')
 var x = 0; var y = 0
-
-
 interact(box)
   .draggable({
     modifiers: [
@@ -22,35 +22,18 @@ interact(box)
   })
 
 rotationAngle = 0
-interact('.box-class')
+interact('.brick-class')
   .on('contextmenu', function (event) {
   rotationAngle = (rotationAngle + 20) % 360;
   event.currentTarget.style.transform = `translate(${x}px, ${y}px) rotate(${rotationAngle}deg)`; 
   });
 
+const addBrick = () => {
+  let div = `<div class="brick-class">THE BRICK</div>`;
+  $('body').append(div);
+}
 
-
-  var ball = document.getElementById('the-ball')
-  var x = 100; var y = 100
-  interact(ball)
-  .draggable({
-    modifiers: [
-      interact.modifiers.snap({
-        targets: [ 
-          interact.snappers.grid({ x: 30, y: 30 })   
-        ],
-        range: Infinity,  
-        relativePoints: [ { x: 0, y: 0 } ]
-      })
-    ]
-  })
-  .on('dragmove', function (event) {
-    x += event.dx
-    y += event.dy
-
-    event.target.style.transform = `translate(${x}px, ${y}px) rotate(${rotationAngle}deg)`
-  })
-
+$('#add-brick').click(addBrick);
 
 
 
