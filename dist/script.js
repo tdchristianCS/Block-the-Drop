a7495e4eece2babfb16fc6f268d2f5d25014169e
Kyle Flowers <kyle.w.flowers@gmail.com>
2025-05-06 14:22:58 -0400

    hover boxes

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,8 @@
 var box = document.getElementById('the-brick')
 var x = 0; var y = 0
 var rotationAngle = 0
+
+//Dragging Box
 interact('.brick-class')
   .draggable({
     modifiers: [
@@ -22,6 +24,7 @@ interact('.brick-class')
     event.target.style.transform = `translate(${x}px, ${y}px) rotate(${rotationAngle}deg)`
   })
 
+//Rotating Box
 rotationAngle = 0
 interact('.brick-class')
   .on('contextmenu', function (event) {
@@ -29,14 +32,12 @@ interact('.brick-class')
   event.currentTarget.style.transform = `translate(${x}px, ${y}px) rotate(${rotationAngle}deg)`; 
   });
 
+//Adding New Brick
 const addBrick = () => {
   let div = `<div class="brick-class">THE BRICK</div>`;
   $('body').append(div);
-// make new brick draggable and rotatable
 }
-
 $('#add-brick').click(addBrick);
-
 
 
 
